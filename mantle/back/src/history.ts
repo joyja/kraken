6770798d053f96c70ff95e253277f5153703fcb6
James Joy <joyja@jarautomation.io>
2023-08-21 22:59:20 +0000

    added custom table name to mantle

--- mantle/back/src/history.ts
+++ mantle/back/src/history.ts
@@ -6,6 +6,7 @@ const log = new Log('history')
 
 export class History {
   private pool:Pool
+  private tableName = process.env.MANTLE_HISTORY_TABLENAME || 'history'
   private constructor(pool:Pool) {
     this.pool = pool
   }
@@ -15,7 +16,7 @@ export class History {
     return history
   }
   private async createTableIfNotExists() {
-    let sql = `CREATE TABLE IF NOT EXISTS "history" (
+    let sql = `CREATE TABLE IF NOT EXISTS "${this.tableName}" (
       "group_id" TEXT,
       "node_id" TEXT,
       "device_id" TEXT,
@@ -27,14 +28,14 @@ export class History {
       "timestamp" TIMESTAMPTZ
     )`
     await this.pool.query(sql)
-    sql = `SELECT create_hypertable('history','timestamp',if_not_exists => TRUE)`
+    sql = `SELECT create_hypertable('${this.tableName}','timestamp',if_not_exists => TRUE)`
     await this.pool.query(sql)
   }
   async log(metric:SparkplugMetric) {
     let valueColumn = metric.type.startsWith('Int') ? 'int_value' : metric.type === 'Float' ? 'float_value' : metric.type === 'String' ? 'string_value' : metric.type === 'Boolean' ? 'bool_value' : null
     let value = metric.value
     if (valueColumn && value) {
-      let sql = `INSERT INTO history ("group_id", "node_id", "device_id", "metric_id", "${valueColumn}", timestamp) VALUES ($1, $2, $3, $4, $5, $6)`
+      let sql = `INSERT INTO "${this.tableName}" ("group_id", "node_id", "device_id", "metric_id", "${valueColumn}", timestamp) VALUES ($1, $2, $3, $4, $5, $6)`
       let params = [metric.groupId, metric.nodeId, metric.deviceId, metric.id, value, metric.updatedOn]
       this.pool.query(sql, params)
     } else {
@@ -42,7 +43,7 @@ export class History {
     }
   }
   async getHistory({ metric, start, end }:{ metric:SparkplugMetric, start:Date, end:Date }) {
-    let sql = `SELECT * FROM history WHERE "group_id" = $1 AND "node_id" = $2 AND "device_id" = $3 AND "metric_id" = $4 AND "timestamp" >= $5 AND "timestamp" <= $6 ORDER BY "timestamp" ASC`
+    let sql = `SELECT * FROM "${this.tableName}" WHERE "group_id" = $1 AND "node_id" = $2 AND "device_id" = $3 AND "metric_id" = $4 AND "timestamp" >= $5 AND "timestamp" <= $6 ORDER BY "timestamp" ASC`
     let params = [metric.groupId, metric.nodeId, metric.deviceId, metric.id, start, end]
     let result = await this.pool.query(sql, params)
     return result.rows
