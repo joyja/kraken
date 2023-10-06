239a598ffed5ec2869a14a5711583c7b789a91eb
James A. Joy, PE <38839768+joyja@users.noreply.github.com>
2023-10-05 17:44:55 -0700

    added alarms query

--- mantle/back/src/resolvers/Query/index.ts
+++ mantle/back/src/resolvers/Query/index.ts
@@ -1,7 +1,12 @@
 import { spdata } from "../../mqtt"
+import { alarms } from "../../alarms"
 
 export function info() { return 'Data Acquisition and visualation for tentacle-edge devices.'}
 
 export function groups() {
   return spdata.groups
+}
+
+export function alarms() {
+  return alarms.getAlarms()
 }
