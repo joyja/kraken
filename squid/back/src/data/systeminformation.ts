186d7c0ffd44d645de8e7c47c2c67103293acf07
James Joy <joyja@jarautomation.io>
2023-12-13 07:51:32 +0000

    added disk usage and gigabyte stats

--- squid/back/src/data/systeminformation.ts
+++ squid/back/src/data/systeminformation.ts
@@ -5,55 +5,79 @@ export class System extends MQTTData {
   constructor() {
     const metrics:MqttDataMetric[] = [{
         name: 'systemInformation/os/platform',
-        getter: async () => si.osInfo().then(data => data.platform),
+        getter: async () => si.osInfo().then((data:any) => data.platform),
         type: 'String'
       },{
         name: 'systemInformation/os/distro',
-        getter: async () => si.osInfo().then(data => data.distro),
+        getter: async () => si.osInfo().then((data:any) => data.distro),
         type: 'String'
       },{
         name: 'systemInformation/os/release',
-        getter: async () => si.osInfo().then(data => data.release),
+        getter: async () => si.osInfo().then((data:any) => data.release),
         type: 'String'
       },{
         name: 'systemInformation/os/arch',
-        getter: async () => si.osInfo().then(data => data.arch),
+        getter: async () => si.osInfo().then((data:any) => data.arch),
         type: 'String'
       },{
         name: 'systemInformation/os/hostname',
-        getter: async () => si.osInfo().then(data => data.hostname),
+        getter: async () => si.osInfo().then((data:any) => data.hostname),
         type: 'String'
       },{
         name: 'systemInformation/os/fqdn',
-        getter: async () => si.osInfo().then(data => data.fqdn),
+        getter: async () => si.osInfo().then((data:any) => data.fqdn),
         type: 'String'
       },{
         name: 'systemInformation/cpu/processors',
-        getter: async () => si.cpu().then(data => data.processors),
+        getter: async () => si.cpu().then((data:any) => data.processors),
         type: 'Float'
       },{
         name: 'systemInformation/cpu/physicalCores',
-        getter: async () => si.cpu().then(data => data.physicalCores),
+        getter: async () => si.cpu().then((data:any) => data.physicalCores),
         type: 'Float'
       },{
         name: 'systemInformation/cpu/cores',
-        getter: async () => si.cpu().then(data => data.cores),
+        getter: async () => si.cpu().then((data:any) => data.cores),
         type: 'Float'
       },{
         name: 'systemInformation/avgLoad',
-        getter: async () => si.currentLoad().then(data => data.avgLoad),
+        getter: async () => si.currentLoad().then((data:any) => data.avgLoad),
         type: 'Float'
       },{
         name: 'systemInformation/mem/used',
-        getter: async () => si.mem().then(data => data.used),
+        getter: async () => si.mem().then((data:any) => data.used),
+        type: 'Float'
+      },{
+        name: 'systemInformation/mem/used/gigabytes',
+        getter: async () => si.mem().then((data:any) => data.used / (1024 * 1024 * 1024)),
         type: 'Float'
       },{
         name: 'systemInformation/mem/total',
-        getter: async () => si.mem().then(data => data.total),
+        getter: async () => si.mem().then((data:any) => data.total),
+        type: 'Float'
+      },{
+        name: 'systemInformation/mem/total/gigabytes',
+        getter: async () => si.mem().then((data:any) => data.total / (1024 * 1024 * 1024)),
         type: 'Float'
       },{
         name: 'systemInformation/temp',
-        getter: async () => si.cpuTemperature().then(data => data.main),
+        getter: async () => si.cpuTemperature().then((data:any) => data.main),
+        type: 'Float'
+      },{
+        name: 'systemInformation/disk/size',
+        getter: async () => si.fsSize().then((data:any) => data[0].size),
+        type: 'Float'
+      },{
+        name: 'systemInformation/disk/size/gigabytes',
+        getter: async () => si.fsSize().then((data:any) => data[0].size / (1024 * 1024 * 1024)),
+        type: 'Float'
+      },{
+        name: 'systemInformation/disk/used',
+        getter: async () => si.fsSize().then((data:any) => data[0].used),
+        type: 'Float'
+      },{
+        name: 'systemInformation/disk/used/gigabytes',
+        getter: async () => si.fsSize().then((data:any) => data[0].used / (1024 * 1024 * 1024)),
         type: 'Float'
     }]
     super(metrics)
